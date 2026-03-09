00f1886478dce5d6b9cb586770ba20906f6519e1
miguel-angel-perez-vargas <miguelangelperezvargas04@gmail.com>
2026-03-08 23:07:17 -0500

    Primeros 3 puntos completados de los test de Geometric

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -1,3 +1,4 @@
+import math
 class Geometria:
     """
     Class with geometric exercises.
@@ -5,42 +6,24 @@ class Geometria:
     """
     
     def area_rectangulo(self, base, altura):
-        """
-        Calcula el área de un rectángulo.
-        
-        Args:
-            base (float): Longitud de la base del rectángulo
-            altura (float): Altura del rectángulo
-            
-        Returns:
-            float: Área del rectángulo
-        """
-        return base*altura
-    
+
+        if base <= 0 or altura <= 0:
+            return 0
+        return base * altura
+       
     def perimetro_rectangulo(self, base, altura):
-        """
-        Calcula el perímetro de un rectángulo.
-        
-        Args:
-            base (float): Longitud de la base del rectángulo
-            altura (float): Altura del rectángulo
-            
-        Returns:
-            float: Perímetro del rectángulo
-        """
-        pass
-    
+         
+         if base <= 0 or altura <= 0:
+            return 0
+         return 2 * (base + altura)
+              
+           
     def area_circulo(self, radio):
-        """
-        Calcula el área de un círculo.
+         if radio <= 0:
+             return 0
+         return math.pi * radio ** 2
+
         
-        Args:
-            radio (float): Radio del círculo
-            
-        Returns:
-            float: Área del círculo
-        """
-        pass
     
     def perimetro_circulo(self, radio):
         """
